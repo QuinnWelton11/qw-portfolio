fba7be483cfa3d149d5c7a92ef74786133eb8d3b
Quinn Welton <quinn.welton@gmail.com>
2025-10-30 01:09:12 -0700

    Refactor styles and update asset links in documentation
    - Deleted unused CSS file: index-_haz7C3I.css
    - Updated script and stylesheet links in index.html to new asset versions
    - Adjusted Header component's margin to use auto for horizontal centering

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import Nav from "./Nav";
 function Header() {
   return (
     <>
-      <header className="text-white bg-slate-900/10 backdrop-blur-sm top-0 flex-wrap z-20  flex max-w-6xl items-center justify-between border-2 border-cyan-300/20 pl-4 pr-10 py-5 rounded-full mt-5 mb-5 mx-3">
+      <header className="text-white bg-slate-900/10 backdrop-blur-sm top-0 flex-wrap z-20 flex max-w-6xl items-center justify-between border-2 border-cyan-300/20 pl-4 pr-10 py-5 rounded-full mt-5 mb-5 mx-auto">
         <Logo />
         <Nav />
       </header>
