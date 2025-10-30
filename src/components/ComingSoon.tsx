4176a2b1dd93ec34018acaa0f7d40c8368a6339d
Quinn Welton <quinn.welton@gmail.com>
2025-10-30 01:27:44 -0700

    Refactor styles and update assets for Coming Soon page
    - Deleted unused CSS file: index-DVSp3yqR.css
    - Updated HTML to link new JavaScript and CSS files: index-DHSl1dgS.js and index-BscWkutK.css
    - Modified Coming Soon component styles for improved layout and responsiveness

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -2,8 +2,8 @@ import { Construction } from "lucide-react";
 
 function ComingSoon() {
   return (
-    <div className="text-white flex flex-col md:flex-row justify-center max-w-6xl mx-4 bg-slate-900/20 backdrop-blur-sm border-2 border-cyan-300/20 shadow-cyan-300/10 shadow-md rounded-2xl l p-4 gap-3 mx-auto my-[25%]">
-      <div className="text-center flex justify-center">
+    <div className="text-white flex flex-col md:flex-row justify-center max-w-4xl  bg-slate-900/20 backdrop-blur-sm border-2 border-cyan-300/20 shadow-cyan-300/10 shadow-md rounded-xl l p-4 gap-3 mx-auto my-[25%]">
+      <div className="text-center flex justify-center gap-10 items-center">
         <Construction size={50} />
         <h1>
           This page is currently under construction! Check back soon to see the
