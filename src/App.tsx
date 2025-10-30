c9296b00c176d9a3568784883374e6cdbf16a324
Quinn Welton <quinn.welton@gmail.com>
2025-10-30 01:10:33 -0700

    Refactor code structure for improved readability and maintainability

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
         muted
       />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/qw-portfolio" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/stack" element={<Stack />} />
