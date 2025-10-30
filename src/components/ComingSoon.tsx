3e9aebd35f87fde4ba41efd10a895dd95d8a78bf
Quinn Welton <quinn.welton@gmail.com>
2025-10-30 01:14:58 -0700

    Refactor code structure for improved readability and maintainability

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -2,7 +2,7 @@ import { Construction } from "lucide-react";
 
 function ComingSoon() {
   return (
-    <div className="text-white flex flex-col md:flex-row justify-center max-w-6xl mx-4 bg-slate-900/20 backdrop-blur-sm border-2 border-cyan-300/20 shadow-cyan-300/10 shadow-md rounded-2xl l p-4 gap-3 my-[25%]">
+    <div className="text-white flex flex-col md:flex-row justify-center max-w-6xl mx-4 bg-slate-900/20 backdrop-blur-sm border-2 border-cyan-300/20 shadow-cyan-300/10 shadow-md rounded-2xl l p-4 gap-3 mx-auto my-[25%]">
       <div className="text-center flex justify-center">
         <Construction size={50} />
         <h1>
