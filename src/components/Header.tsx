57704b3088596c5ae4d3a696ab796e7dcb905dbf
Quinn Welton <quinn.welton@gmail.com>
2025-10-30 01:35:09 -0700

    Refactor styles and update header component
    - Deleted old CSS file `index-BscWkutK.css` and added new CSS file `index-DU4ZEo3s.css`.
    - Updated script source in `index.html` to use new JavaScript file `index-B1-wWWXf.js`.
    - Modified `Header.tsx` component styles for improved design:
      - Changed background color to `bg-blue-400/10`.
      - Adjusted border color to `border-cyan-400/20`.
      - Added shadow effect with `shadow-cyan-400/20`.
      - Wrapped logo and navigation in a flex container for better alignment.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,11 @@ import Nav from "./Nav";
 function Header() {
   return (
     <>
-      <header className="text-white bg-slate-900/10 backdrop-blur-sm top-0 flex-wrap z-20 flex max-w-6xl items-center justify-between border-2 border-cyan-300/20 pl-4 pr-10 py-5 rounded-full mt-5 mb-5 mx-auto">
-        <Logo />
-        <Nav />
+      <header className="text-white bg-blue-400/10 backdrop-blur-sm z-20 pl-4 pr-10 py-5 mb-5 border-b-2 border-cyan-400/20 shadow-cyan-400/20 shadow-md">
+        <div className="flex flex-row flex-wrap justify-between items-center max-w-6xl mx-auto md:px-4">
+          <Logo />
+          <Nav />
+        </div>
       </header>
     </>
   );
